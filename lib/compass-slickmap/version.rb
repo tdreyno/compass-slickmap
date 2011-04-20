bf472f22f2b5827c14c97c986e45417dddc0c52d
Thomas Reynolds <tdreyno@gmail.com>
2011-04-20 13:32:22 -0700

    reorg

---
+++ lib/compass-slickmap/version.rb
@@ -0,0 +1,5 @@
+module Compass
+  module Slickmap
+    VERSION = "0.5.0"
+  end
+end
--- lib/slickmap.rb
+++
@@ -1,3 +0,0 @@
-Compass::Frameworks.register('slickmap',
-                             :stylesheets_directory => File.join(File.dirname(__FILE__), 'stylesheets'),
-                             :templates_directory   => File.join(File.dirname(__FILE__), 'templates'))
